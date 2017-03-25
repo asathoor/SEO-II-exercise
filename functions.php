f4dab098834355681138b92f55950828a21686c1
asathoor <asathoor@gmail.com>
2017-03-25 08:49:24 +0100

    Add common SEO metatags

--- functions.php
+++ functions.php
@@ -14,24 +14,30 @@ function petj_add_my_metatags(){
 
 	// store repeated info in variables
 	$author = "Per Jensen";
-																
+	$keywords = "WordPress, Tutorials, PHP, MySQL, Linux";
 	?>
 
-	<!-- SEO ( from my hook ) -->
+	<!-- SEO ( from my function ) -->
+
+	<!-- On WordPress and metatags, see @url: https://codex.wordpress.org/Meta_Tags_in_WordPress -->
 	
 	<!-- name via bloginfo -->
 	<meta name="application-name" content="<?php bloginfo('name'); ?>" />
 
 	<!-- use bloginfo or perhaps a variable -->
+	<!-- it's better to use wp standards than to hardcode information -->
+	<meta name="resource-type" content="document" />
 	<meta name="author" content="<?php echo $author; ?>" />
-	<meta name="distributor" content="Multimusen.dk" />
+	<meta name="contact" content="petj@eaaa.dk" />
+	<meta name="copyright" content="Copyright (c) GPLv2 2017 <?php echo $author; ?>" />
+	<meta name="distributor" content="<?php bloginfo( 'name' ); ?>" />
 	<meta name="robots" content="Index" />
-	<meta name="description" content="Tips and tutorials for multimedia developers. ---- <?php wp_title(); ?>"  />
-	<meta name="keywords" content="WordPress, PHP, MySQL, Jquery JavaScript, Flash, ActionScript, LAMP, XAMPP" />
-	<meta name="generator" content="META Tag Generator: http://toolswebtop.com/seo/metatag" />
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>"  />
+	<meta name="keywords" content="<?php echo $keywords; ?>" />
 	<meta name="rating" content="General" />
 
 	<!-- DC Meta-data, url: http://dublincore.org/documents/dcmi-terms/ -->
+	<!-- From here the info is hardcoded. Edit to wp standards, please -->
 	<meta name="dcterms.title" content="Multimusen.dk " />
 	<meta name="dcterms.contributor" content="Per Thykjaer Jensen" />
 	<meta name="dcterms.creator" content="Per Thykjaer Jensen" />
